0bcd2ff1e2965b239c897b813431a8ff0395f1cd
Steven Bruce <brucesc726@gmail.com>
2019-10-17 23:02:32 -0400

    just to push changes, nothing important

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'charlie-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'charlie';
+export class AppComponent implements OnInit {
+  
+  ngOnInit(): void {
+    throw new Error("Method not implemented.");
+  }
 }
